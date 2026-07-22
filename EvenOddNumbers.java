d03aae4789d8050d998f6b91288eca0f98c3024d
Sarthak <sarthak3528.beai25@chitkara.edu.in>
2026-07-22 09:40:38 +0530

    Add files via upload
    PRACTICAL DAY 5

---
+++ EvenOddNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EvenOddNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the limit: ");
+        int n = sc.nextInt();
+
+        System.out.println("Even Numbers:");
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0)
+                System.out.print(i + " ");
+        }
+
+        System.out.println("\nOdd Numbers:");
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0)
+                System.out.print(i + " ");
+        }
+    }
+}
